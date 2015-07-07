ae3fc37d561c44af4d592309e26cf66a775c0642
ShaoWei Teo <Teo_ShaoWei@outlook.com>
2015-07-07 23:24:15 +0800

    Refactor.
    Rename the global variables to ALL_CAPS.  This make it easier to differentiate the global variables from the local variables in functions.

--- src/SolveSudoku.jl
+++ src/SolveSudoku.jl
@@ -1,47 +1,47 @@
 module SudokuSolver
 
-InBlock = Array(Int, 81); InRow = Array(Int, 81); InCol = Array(Int, 81)
+IN_BLOCK = Array(Int, 81); IN_ROW = Array(Int, 81); IN_COL = Array(Int, 81)
 
 const BLANK = 0
 const ONES = 0x3fe
 
-Entry = Array(Int,81)
-Block = Array(Int, 9); Row = Array(Int, 9); Col = Array(Int, 9)
+ENTRY = Array(Int,81)
+BLOCK = Array(Int, 9); ROW = Array(Int, 9); COL = Array(Int, 9)
 
-SeqPtr = 1
-Sequence = Array(Int, 81)
+SEQ_PTR = 1
+SEQUENCE = Array(Int, 81)
 
-Count = 0
-LevelCount = Array(Int, 82)
+COUNT = 0
+LEVEL_COUNT = Array(Int, 82)
 
 
 function SwapSeqEntries(S1::Int, S2::Int)
-    temp = Sequence[S2]
-    Sequence[S2] = Sequence[S1]
-    Sequence[S1] = temp
+    temp = SEQUENCE[S2]
+    SEQUENCE[S2] = SEQUENCE[S1]
+    SEQUENCE[S1] = temp
 end
 
 
 function InitEntry(i::Int, j::Int, val::Int)
-    global SeqPtr
+    global SEQ_PTR
 
     Square = 9(i - 1) + j
     valbit = 1 << val
 
     # Add suitable checks for data consistency.
 
-    Entry[Square] = valbit
-    Block[InBlock[Square]] &= ~valbit
-    Col[InCol[Square]] &= ~valbit #Simpler Col[j] &= ~valbit
-    Row[InRow[Square]] &= ~valbit #Simpler Row[i] &= ~valbit
+    ENTRY[Square] = valbit
+    BLOCK[IN_BLOCK[Square]] &= ~valbit
+    COL[IN_COL[Square]] &= ~valbit #Simpler COL[j] &= ~valbit
+    ROW[IN_ROW[Square]] &= ~valbit #Simpler ROW[i] &= ~valbit
 
-    SeqPtr2 = SeqPtr
-    while SeqPtr2 ≤ 81 && Sequence[SeqPtr2] != Square
+    SeqPtr2 = SEQ_PTR
+    while SeqPtr2 ≤ 81 && SEQUENCE[SeqPtr2] != Square
         SeqPtr2 += 1
     end
 
-    SwapSeqEntries(SeqPtr, SeqPtr2)
-    SeqPtr += 1
+    SwapSeqEntries(SEQ_PTR, SeqPtr2)
+    SEQ_PTR += 1
 end
 
 
@@ -52,7 +52,7 @@ function PrintArray()
         (i % 3 == 1) && println()
         for j in 1:9
             (j % 3 == 1) && print(' ')
-            valbit = Entry[Square]
+            valbit = ENTRY[Square]
             Square += 1
             if valbit == 0
                 ch = '-'
@@ -73,7 +73,7 @@ end
 
 function ConsoleInput()
     for i in 1:9
-        @printf "Row[%d] : " i
+        @printf "ROW[%d] : " i
         InputString = readline(STDIN)
 
         for j in 1:9
@@ -92,15 +92,15 @@ function PrintStats()
     @printf "\nLevel Counts:\n\n"
 
     S = 1
-    while LevelCount[S] == 0
+    while LEVEL_COUNT[S] == 0
         S += 1
     end
 
     i = 1
 
     while S ≤ 81
-        Seq = Sequence[S]
-        @printf "(%d, %d):%4d " cld(Seq, 9) rem1(Seq, 9) LevelCount[S]
+        Seq = SEQUENCE[S]
+        @printf "(%d, %d):%4d " cld(Seq, 9) rem1(Seq, 9) LEVEL_COUNT[S]
         if (i > 5)
             @printf "\n"
             i = 1
@@ -110,7 +110,7 @@ function PrintStats()
         S += 1
     end
 
-    @printf "\n\nCount = %d\n" Count
+    @printf "\n\nCOUNT = %d\n" COUNT
 end
 
 
@@ -125,8 +125,8 @@ function NextSeq(S::Int)
     MinBitCount = 100
 
     for T in S:81
-        Square = Sequence[T]
-        Possibles = Block[InBlock[Square]] & Row[InRow[Square]] & Col[InCol[Square]]
+        Square = SEQUENCE[T]
+        Possibles = BLOCK[IN_BLOCK[Square]] & ROW[IN_ROW[Square]] & COL[IN_COL[Square]]
         BitCount = 0
         while Possibles != 0
             Possibles &= ~(Possibles & -Possibles)
@@ -144,10 +144,10 @@ end
 
 
 function Place(S::Int)
-    global Count
+    global COUNT
 
-    LevelCount[S] += 1
-    Count += 1
+    LEVEL_COUNT[S] += 1
+    COUNT += 1
 
     if S > 81
         Succeed()
@@ -157,27 +157,27 @@ function Place(S::Int)
     S2 = NextSeq(S)
     SwapSeqEntries(S, S2)
 
-    Square = Sequence[S]
+    Square = SEQUENCE[S]
 
-    BlockIndex = InBlock[Square]
-    RowIndex = InRow[Square]
-    ColIndex = InCol[Square]
+    BlockIndex = IN_BLOCK[Square]
+    RowIndex = IN_ROW[Square]
+    ColIndex = IN_COL[Square]
 
-    Possibles = Block[BlockIndex] & Row[RowIndex] & Col[ColIndex]
+    Possibles = BLOCK[BlockIndex] & ROW[RowIndex] & COL[ColIndex]
     while Possibles != 0
         valbit = Possibles & (-Possibles) #Lowest 1 bit in Possibles.
         Possibles &= ~valbit
-        Entry[Square] = valbit
-        Block[BlockIndex] &= ~valbit
-        Row[RowIndex] &= ~valbit
-        Col[ColIndex] &= ~valbit
+        ENTRY[Square] = valbit
+        BLOCK[BlockIndex] &= ~valbit
+        ROW[RowIndex] &= ~valbit
+        COL[ColIndex] &= ~valbit
 
         Place(S + 1)
 
-        Entry[Square] = BLANK #Could be moved out of the loop.
-        Block[BlockIndex] |= valbit
-        Row[RowIndex] |= valbit
-        Col[ColIndex] |= valbit
+        ENTRY[Square] = BLANK #Could be moved out of the loop.
+        BLOCK[BlockIndex] |= valbit
+        ROW[RowIndex] |= valbit
+        COL[ColIndex] |= valbit
     end
 
     SwapSeqEntries(S, S2)
@@ -188,27 +188,27 @@ end
 function main()
     for i in 1:9, j in 1:9
         Square = 9(i - 1) + j
-        InRow[Square] = i
-        InCol[Square] = j
-        InBlock[Square] = 3 * (cld(i, 3) - 1) + cld(j, 3)
+        IN_ROW[Square] = i
+        IN_COL[Square] = j
+        IN_BLOCK[Square] = 3 * (cld(i, 3) - 1) + cld(j, 3)
     end
 
     for Square in 1:81
-        Sequence[Square] = Square
-        Entry[Square] = BLANK
-        LevelCount[Square] = 0
+        SEQUENCE[Square] = Square
+        ENTRY[Square] = BLANK
+        LEVEL_COUNT[Square] = 0
     end
 
     for i in 1:9
-        Block[i] = Row[i] = Col[i] = ONES
+        BLOCK[i] = ROW[i] = COL[i] = ONES
     end
 
-    global SeqPtr = 1
-    global Count = 0
+    global SEQ_PTR = 1
+    global COUNT = 0
 
     ConsoleInput()
-    Place(SeqPtr)
-    @printf "\n\nTotal Count = %d\n" Count
+    Place(SEQ_PTR)
+    @printf "\n\nTotal COUNT = %d\n" COUNT
 
     return 0
 end
